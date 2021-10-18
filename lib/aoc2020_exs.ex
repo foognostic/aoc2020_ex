8aeb5c4ec0a68752f4ac2524f4dbd8a157aaa787
foognostic <seth@foognostic.sh>
2021-10-18 18:41:32 +0000

    Solves day01 star 1
    + super gross

--- lib/aoc2020_exs.ex
+++ lib/aoc2020_exs.ex
@@ -26,4 +26,27 @@ defmodule Aoc2020Exs do
     present_lines = Enum.filter(lines, fn x -> String.length(x) > 0 end)
     Enum.map(present_lines, &String.to_integer/1)
   end
+
+  def solve_day01_star1(numbers) do
+    # hello n^2
+    # hello assumptions that the input data has no collisions on sum 2020
+    Enum.reduce(numbers, 0, fn outer, o_memo ->
+      if o_memo > 0 do
+        o_memo
+      else
+        Enum.reduce(numbers, 0, fn inner, i_memo ->
+          if i_memo > 0 do
+            i_memo
+          else
+            match = 2020 == outer + inner
+            if match do
+              outer * inner
+            else
+              0
+            end
+          end
+        end)
+      end
+    end)
+  end
 end
--- test/aoc2020_exs_test.exs
+++ test/aoc2020_exs_test.exs
@@ -10,4 +10,9 @@ defmodule Aoc2020ExsTest do
     assert [1721, 979, 366, 299, 675, 1456] ==
       Aoc2020Exs.read_exercise_data(:day01, "sample.txt")
   end
+
+  test "solves the sample input" do
+    assert 514579 ==
+      Aoc2020Exs.solve_day01_star1(Aoc2020Exs.read_exercise_data(:day01, "sample.txt"))
+  end
 end
