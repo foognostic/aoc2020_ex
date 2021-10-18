0a6ca7996d0f27e72b23a2027834b345bf210031
foognostic <seth@foognostic.sh>
2021-10-18 17:12:06 +0000

    Initial commit

---
+++ lib/aoc2020_exs.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2020Exs do
+  @moduledoc """
+  Documentation for `Aoc2020Exs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2020Exs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2020_exs_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2020ExsTest do
+  use ExUnit.Case
+  doctest Aoc2020Exs
+
+  test "greets the world" do
+    assert Aoc2020Exs.hello() == :world
+  end
+end
